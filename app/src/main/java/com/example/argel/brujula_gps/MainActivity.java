3b08272c308cd3d45a16567b2d0daa9515caa1c0
El Mejorge <jorge.montiel@cetys.edu.mx>
2017-12-06 23:41:41 -0800

    Team Aaron Burr

--- app/src/main/java/com/example/argel/brujula_gps/MainActivity.java
+++ app/src/main/java/com/example/argel/brujula_gps/MainActivity.java
@@ -16,6 +16,8 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.TextView;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -24,16 +26,20 @@ import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Set;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    public TextView txtDegree, txtLat, txtAlt, txtHora, txtDia;
+    public TextView txtDegree, txtLat, txtAlt, txtHora, txtDia, txtAngX, txtAngY;
     public Location loc;
     public LocationManager myLocationManager;
-    public CustomLocationListener myLocationListener;
+    public double altCoord, latCoord;
+        public CustomLocationListener myLocationListener;
     public Criteria criteria;
     private float currentDegree = 0f;
     private SensorManager mSensorManager;
@@ -45,23 +51,96 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private BluetoothSocket socket;
     private OutputStream outputStream;
 
+    private int currentHour = 1;
+    private int currentMonth = 1;
+    private int currentDay =1;
+
+    private int xDegree = 0;
+    private int yDegree = 0;
+
+    private String currentTimeString = "";
+    private String currentDateString = "";
+
+    private boolean isXDegInc = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String currentDateString = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-        String currentTimeString = new SimpleDateFormat("HH:mm:ss").format(new Date());
+
         txtDegree = findViewById(R.id.Contenido_degree);
         txtLat = findViewById(R.id.Contenido_latitude);
         txtAlt = findViewById(R.id.Contenido_altitude);
         txtHora = findViewById(R.id.Hora_content);
         txtDia = findViewById(R.id.Dia_content);
+        txtAngX = findViewById(R.id.txtAngX);
+        txtAngY = findViewById(R.id.txtAngY);
 
-        txtHora.setText(currentTimeString);
-        txtDia.setText(currentDateString);
+        setSocket();
 
+        setLocationManager();
+
+        sendDegrees();
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+    }
+
+    public class CustomLocationListener implements LocationListener {
+
+       public void onLocationChanged(Location location) {
+            try{
+                myLocationManager.removeUpdates(myLocationListener);
+
+                setLocationTexts(location);
+                setDateTexts();
+
+                xDegree = (int) getHorizontalDegree();
+                yDegree = (int) getVerticalDegree();
+
+                sendDegrees();
+            }catch(SecurityException e){
+                e.printStackTrace();
+            }
+
+        }
+
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        public void onProviderEnabled(String s) {
+
+        }
+
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+    public void onSensorChanged(SensorEvent event){
+        float degree = Math.round(event.values[0]);
+        txtDegree.setText(Float.toString(degree));
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+        // for the system's orientation sensor registered listeners
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+
+    }
+
+    public void setSocketAction(View view){
+        setSocket();
+    }
+
+    private void setSocket() {
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
         if(bluetoothAdapter == null)
         {
             Toast.makeText(getApplicationContext(), "No Bluetooth", Toast.LENGTH_SHORT).show();
@@ -90,9 +169,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
 
         try {
-            socket = device.createRfcommSocketToServiceRecord(PORT_UUID);
+            socket =(BluetoothSocket) device.getClass().getMethod("createRfcommSocket", new Class[] {int.class}).invoke(device,1);
             socket.connect();
-        } catch (IOException e) {
+        } catch (IOException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            makeToast("Bluetooth done goof'd.");
             e.printStackTrace();
         }
 
@@ -101,65 +181,137 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        String datos = "a";
-        try {
-            outputStream.write(datos.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-
+    private void setLocationManager() {
         myLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         myLocationListener = new CustomLocationListener();
 
         try{
             myLocationManager.requestLocationUpdates(myLocationManager.NETWORK_PROVIDER, 5, 1, myLocationListener);
-            Log.d("Prueba", "Entra");
         }catch(SecurityException e){
             e.printStackTrace();
         }
-
     }
 
-    public class CustomLocationListener implements LocationListener {
-
-       public void onLocationChanged(Location location) {
-            try{
-                myLocationManager.removeUpdates(myLocationListener);
-                txtLat.setText(Double.toString(location.getLatitude()));
-                txtAlt.setText(Double.toString(location.getAltitude()));
-            }catch(SecurityException e){
-                e.printStackTrace();
-            }
-
-        }
-
-        public void onStatusChanged(String s, int i, Bundle bundle) {
-
-        }
-
-        public void onProviderEnabled(String s) {
-
-        }
-
-        public void onProviderDisabled(String s) {
-
-        }
-    };
-    public void onSensorChanged(SensorEvent event){
-        float degree = Math.round(event.values[0]);
-        txtDegree.setText(Float.toString(degree));
+    private double getHorizontalDegree() //ASIMUTAL
+    {
+        // TODO Change to degrees
+        double degCosNum = (getSinFromDegree(getDeclinationDegree())-(getSinFromDegree(latCoord)*getSinFromDegree(altCoord)));
+        double degCosDen = (getCosFromDegree(latCoord)*getCosFromDegree(altCoord));
+        return degCosNum/degCosDen;
     }
-    protected void onResume() {
-        super.onResume();
 
-        // for the system's orientation sensor registered listeners
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
-                SensorManager.SENSOR_DELAY_GAME);
+    private double getDeclinationDegree()
+    {
+        return getCosFromDegree((currentDay + 10) * 0.9863) * -23.44; // 0.9863 = 365/360
+                                                                    // -23.44 = inclinacion eje tierra en grados
     }
-    public void onAccuracyChanged(Sensor sensor, int accuracy){
 
+    private double getSinFromDegree(Double degree){
+        return Math.sin(Math.toRadians(degree));
+    }
+
+    private double getCosFromDegree(Double degree){
+        return Math.cos(Math.toRadians(degree));
+    }
+
+    private double getVerticalDegree() {
+        return 0.0;
+    }
+
+    public void sendDegrees() {
+        String datos = xDegree + "," + yDegree;
+
+        try {
+            outputStream.write(datos.getBytes());
+        } catch (IOException e) {
+            makeToast("Bluetooth disconnected.");
+            e.printStackTrace();
+        }
+    }
+
+    private void setLocationTexts(Location location){
+        latCoord = location.getLatitude();
+        altCoord = location.getAltitude();
+
+        txtLat.setText(Double.toString(location.getLatitude()));
+        txtAlt.setText(Double.toString(location.getAltitude()));
+
+        txtAngX.setText(Double.toString(getHorizontalDegree()));
+        txtAngY.setText(Double.toString(getVerticalDegree()));
+    }
+
+    private void setDateTexts(){
+        Calendar now = Calendar.getInstance();
+
+        currentHour = now.get(Calendar.HOUR_OF_DAY);
+        currentMonth = now.get(Calendar.MONTH);
+        currentDay = now.get(Calendar.DAY_OF_YEAR);
+
+//        currentDateString = new SimpleDateFormat("yyyy-MM-dd", Locale.US).format(new Date());
+        currentDateString = Integer.toString(currentMonth);
+//        currentTimeString = new SimpleDateFormat("HH:mm:ss", Locale.US).format(new Date());
+        currentTimeString = Integer.toString(currentHour);
+
+        isXDegInc = currentMonth < 7;
+
+        txtHora.setText(currentTimeString);
+        txtDia.setText(currentDateString);
+    }
+
+    public void addAnHour(View view) {
+        currentHour++;
+
+        yDegree = yDegree + 180/16;
+
+        if(currentHour > 24) {
+            currentHour = 0;
+        }
+
+        if(yDegree > 180){
+            yDegree = 0;
+        }
+
+        txtHora.setText(Integer.toString(currentHour));
+
+        sendDegrees();
+    }
+
+    public void addMonth(View view){
+        currentMonth++;
+
+        if(currentMonth > 12){
+            currentMonth = 1;
+        }
+
+        int step = 180/6;
+
+        if(xDegree + step > 180){
+            isXDegInc = false;
+        }
+
+        if(xDegree - step < 0){
+            isXDegInc = true;
+        }
+
+        if(isXDegInc){
+            xDegree += step;
+        } else {
+            xDegree -= step;
+        }
+
+        txtDia.setText(Integer.toString(currentMonth));
+
+        sendDegrees();
+    }
+
+    private void makeToast(CharSequence msg){
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, msg, duration);
+        toast.setGravity(Gravity.TOP, 0, 150);
+        toast.show();
     }
 }
